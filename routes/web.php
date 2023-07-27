9f90fa5031fa7bc7bcfd2cb208ebba3185f07840
hidrart <61191577+hidrart@users.noreply.github.com>
2023-07-28 04:24:09 +0700

    make migration

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+// SENTRY
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
+// AUTH
+Auth::routes();
